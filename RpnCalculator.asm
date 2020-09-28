911b96feb8577cd33de3888b1091624acab07557
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2020-09-27 21:37:11 -0400

    Fix keypad table description

--- RpnCalculator.asm
+++ RpnCalculator.asm
@@ -2468,9 +2468,9 @@ scanKeyPad:
     ; and return it in register "rKey"
 
     ; Keypad keys are numbered as follows:
-    ;   0   1   2   3
-    ;   4   5   6   7
-    ;   8   9   10  11
+    ;    0   1   2   3
+    ;    4   5   6   7
+    ;    8   9  10  11
     ;   12  13  14  15
 
     ; rKey                      = Returns the number of the key hit
